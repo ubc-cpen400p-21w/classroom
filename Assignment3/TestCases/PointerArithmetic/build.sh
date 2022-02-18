aee8d43da74daee7a77eaefb33b09c7a7f487e64
uditagarwal97 <dev.madaari@gmail.com>
2022-02-18 10:02:42 -0800

    Added Assignment 3

---
+++ Assignment3/TestCases/PointerArithmetic/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+# Before using this script, please set the LLVM_BUILD_DIR environment variable.
+# example: export LLVM_BUILD_DIR=/home/cpen400/llvm-project/build
+
+rm *.bc
+$LLVM_BUILD_DIR/bin/clang -O0 -g -emit-llvm -o PointerArith.bc -c PointerArith.c
